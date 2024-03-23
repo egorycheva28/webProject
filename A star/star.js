20dcb04c0875bfb7819e7d599795557367464c8c
bdvzt <144367629+bdvzt@users.noreply.github.com>
2024-03-24 01:08:29 +0700

    Update star.js

--- A star/star.js
+++ A star/star.js
@@ -18,66 +18,66 @@ function SearchPath()
 
 const cells = []; // создаём массив ячеек лабиринта
 
-for (let x = 0; x < N; x++) { 
-	cells[x] = []; 
-	for (let y = 0; y < N; y++) { 
-		cells[x][y] = null; 
-	} 
-} 
+for (let x = 0; x < N; x++) {
+    cells[x] = [];
+    for (let y = 0; y < N; y++) {
+        cells[x][y] = null;
+    }
+}
 
-class CellA { // класс, который создается для каждой ячейки
-	constructor(x, y) { 
-		this.x = x; 
-		this.y = y; 
-		this.visited = false; 
-		this.walls = { 
-			top: true, 
-			right: true, 
-			bottom: true, 
-			left: true, 
-		}; 
-	} 
+class MazeCell { // класс, который создается для каждой ячейки
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.visited = false;
+        this.walls = {
+            top: true,
+            right: true,
+            bottom: true,
+            left: true,
+        };
+    }
 
-	show() { // здесь рисуются стены у ячейки
-		const x = this.x * cellSize; 
-		const y = this.y * cellSize; 
-    
-		pen.beginPath(); 
+    show() { // здесь рисуются стены у ячейки
+        const x = this.x * cellSize;
+        const y = this.y * cellSize;
 
-		if (this.walls.top) { // рисуем стену наверху
-			pen.moveTo(x, y); 
-			pen.lineTo(x + cellSize, y); 
-		} 
+        pen.beginPath();
 
-		if (this.walls.right) { // рисуем стену справа
-			pen.moveTo(x + cellSize, y); 
-			pen.lineTo(x + cellSize, y + cellSize); 
-		} 
+        if (this.walls.top) { // рисуем стену наверху
+            pen.moveTo(x, y);
+            pen.lineTo(x + cellSize, y);
+        }
 
-		if (this.walls.bottom) { // рисуем стену снизу
-			pen.moveTo(x + cellSize, y + cellSize); 
-			pen.lineTo(x, y + cellSize); 
-		} 
+        if (this.walls.right) { // рисуем стену справа
+            pen.moveTo(x + cellSize, y);
+            pen.lineTo(x + cellSize, y + cellSize);
+        }
 
-		if (this.walls.left) { // // рисуем стену слева
-			pen.moveTo(x, y + cellSize); 
-			pen.lineTo(x, y); 
-		} 
-		pen.strokeStyle = 'green'; // далее стиль ячейки (переделаем)
-		pen.lineWidth = 5; 
-		pen.lineCap = "round"; 
-		pen.stroke(); 
-	} 
-} 
+        if (this.walls.bottom) { // рисуем стену снизу
+            pen.moveTo(x + cellSize, y + cellSize);
+            pen.lineTo(x, y + cellSize);
+        }
+
+        if (this.walls.left) { // // рисуем стену слева
+            pen.moveTo(x, y + cellSize);
+            pen.lineTo(x, y);
+        }
+        pen.strokeStyle = 'green'; // далее стиль ячейки (переделаем)
+        pen.lineWidth = 5;
+        pen.lineCap = "round";
+        pen.stroke();
+    }
+}
 
 
 function genMaze(x, y) { // генерируем лабиринт
-	const currCell = cells[x][y]; // копируем текущую ячейку
-	currCell.visited = true; // отмечаем посещенную ячейку
+    const currCell = cells[x][y]; // копируем текущую ячейку
+    currCell.visited = true; // отмечаем посещенную ячейку
 
-	const directions = randomize(['top', 'right', 'bottom', 'left']); 
+    const directions = randomize(['top', 'right', 'bottom', 'left']);
 
-	for (let i = 0; i < directions.length; i++) {
+    for (let i = 0; i < directions.length; i++) {
         let dx, dy;
 
         if (i === 'top') { // смещение
@@ -114,22 +114,21 @@ function genMaze(x, y) { // генерируем лабиринт
                 } else if (i === 'left') {
                     neighbour.walls['right'] = false;
                 }
-                
+
                 genMaze(newX, newY); // рекурсия для всех ячеек лабиринта
             }
         }
     }
-	generatedMaze = 
-		cells.map(row => row.map( 
-			cell => ({ ...cell }))); 
-	solutionPath = solveMaze(); 
-} 
+    generatedMaze = cells.map(row => row.map(cell => ({ ...cell }))); // клонируем
+
+    solutionPath = solveMaze();
+}
 
 function setup() { // инициализация каждой ячейки лабиринта
-	for (let x = 0; x < rows; x++) { 
-		for (let y = 0; y < cols; y++) { 
-			cells[x][y] = new CellA(x, y); 
-		} 
-	} 
-	genMaze(0, 0); 
+    for (let x = 0; x < N; x++) {
+        for (let y = 0; y < N; y++) {
+            cells[x][y] = new MazeCell(x, y);
+        }
+    }
+    genMaze(0, 0);
 } 
