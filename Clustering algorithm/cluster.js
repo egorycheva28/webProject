5d5e6e5123cd1657aed8500f8ec71ce0b3433129
egorycheva28 <egor.masha28@list.ru>
2024-04-02 15:48:48 +0700

    Clustering algorithm

--- Clustering algorithm/cluster.js
+++ Clustering algorithm/cluster.js
@@ -1,47 +1,126 @@
-/*let canvas=document.getElementById("canvas");
-let context=canvas.getContext("2d");
-let points=[];
-let clusters=[];
-canvas.addEventListener("click",function(event)
-{
-    let rect=canvas.getBoundingClientRect();
-    //let x=event.clientX-rect.left;
-    //let y=event.clientY-rect.top;
-    const x = Math.floor((event.clientX - rect.left)/5);
-    const y = Math.floor((event.clientY - rect.top)/5);
-    //context.beginPath();
-    context.arc(x*5,y*5,5,0,Math.PI*2);
-    context.fill();
-    //context.closePath();
-    //points.push({x,y});
-    //drawPoint(x,y);
-});
-function drawPoint(x,y)
-{
-    context.beginPath();
-    context.fillArc(x,y,5,0,Math.PI*2);
-    //context.fillStyle="black";
-    context.fill();
-    context.closePath();
-}*/
 let canvas=document.getElementById("canvas");
 let context=canvas.getContext("2d");
+
 let points=[];
 let clusters=[];
-function drawPoint(x,y)
-{
-    context.beginPath();
-    context.arc(x,y,5,0,Math.PI*2);
-    //context.fillStyle="black";
-    context.fill();
-    context.stroke();
-}
-canvas.addEventListener("click",function(event)
-{
-    let rect=canvas.getBoundingClientRect();
-    let x=event.clientX-rect.left;
-    let y=event.clientY-rect.top;
-    points.push({x,y});
-    drawPoint(x,y);
-});
+let collorCenter=['red','green','blue','yellow','purple'];
 
+let centers=[];
+let allVector = [];
+let flagPoints = true;
+let flagCenters=true;
+
+canvas.addEventListener("click",function(event){
+    
+    if(flagPoints==true)
+    {
+        let rect=canvas.getBoundingClientRect();
+        let x=event.clientX-rect.left;
+        let y=event.clientY-rect.top;
+        let point = {
+            pointx: x,
+            pointy: y
+        }
+        points.push(point);
+        context.fillStyle = "black";
+        context.beginPath();
+        context.arc(x,y,10,0,2*Math.PI);
+        context.stroke();
+        context.fill();
+    }
+    return points;
+        
+});
+/*canvas.onclick = function (event) { //get dot
+    if (flag === true){
+        //let x = event.offsetX;
+        //let y = event.offsetY;
+        let rect=canvas.getBoundingClientRect();
+        let x=event.clientX-rect.left;
+        let y=event.clientY-rect.top;
+        let dot = {
+            dotx: x,
+            doty: y
+        }
+        points.push(dot);
+        context.fillStyle = "black";
+        context.beginPath();
+        context.arc(x,y,10,0,2*Math.PI);
+        context.stroke();
+        context.fill();
+    }
+    return points;
+}*/
+//document.getElementById("clearButton").onclick = clean;
+//document.getElementById("addCentroidButton").onclick = addCenter;
+//document.getElementById("addCentroidButton").onclick = addCentroid;
+
+function addCenter()
+{
+    let input = document.querySelector('input');
+    let numberCluster=input.value;
+   
+    if(flagCenters==true && points.length!=0 && numberCluster<=points.length)
+    {
+        for(let i=0;i<numberCluster;i++)
+        {
+            let randomCenter=Math.floor(Math.random()*points.length);
+            let Center={
+                X: points[randomCenter].pointx,
+                Y: points[randomCenter].pointy,
+                collor: collorCenter[i],
+            };
+            context.fillStyle=Center.collor;
+            context.beginPath();
+            context.fillRect(Center.X,Center.Y,20,20);
+        
+            centers.push(Center);
+            clusters.push([]);      
+        }
+    
+
+        flagPoints=false;
+        flagCenters=false;
+        return centers; 
+    }
+}
+
+function buildCluster()
+{
+    for(let i=0; i<points.length; i++)
+    { // calculating avg coard for i clusters
+        let singleDotArry = [];
+        for(let j = 0; j<centers.length; j++){
+            let dotCoardX = points[i].pointx - centers[j].coardX;
+            let dotCoardY = points[i].pointy - centers[j].coardY;
+            let vector = Math.pow((Math.pow(dotCoardX,2) + Math.pow(dotCoardY,2)),0.5);
+            singleDotArry.push(vector)
+        }
+        allVector.push(singleDotArry);
+    }
+    for(let i=0; i<allVector.length; i++)
+    {
+        let min = allVector[i][0];
+        let indexMin = 0;
+        for(let j=0 ; j<centers.length; j++)
+        {
+            if(min > allVector[i][j])
+            {
+                min = allVector[i][j];
+                indexMin = j;
+            }
+        }
+        clusters[indexMin].push(points[i]);
+        context.fillStyle = collorCenter[indexMin];
+        context.beginPath();
+        context.arc(points[i].pointx,points[i].pointy,10,0,2*Math.PI);
+        context.stroke();
+        context.fill(); 
+    }
+    return clusters;
+}
+
+function clean()
+{
+    location.reload();
+}
