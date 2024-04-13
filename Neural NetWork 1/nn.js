65d9d03eb5525c69a87b247d72c8ebac88125726
egorycheva28 <egor.masha28@list.ru>
2024-04-13 22:27:05 +0700

    меню добавлено

--- Neural NetWork 1/nn.js
+++ Neural NetWork 1/nn.js
@@ -48,3 +48,12 @@ function clean()
 {
     location.reload();
 }
+
+function show(menu)
+{
+    let element = document.getElementById(menu);
+    if(element)
+    {
+        element.style.display="block";
+    }
+}
